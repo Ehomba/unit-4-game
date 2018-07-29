f42d95c4b5acea842a7990904d7171a01c3ab9c1
Ehomba <Milesbenovic@gmail.com>
2018-07-29 15:03:09 -0500

    some commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+
+console.log("hello")
+
+
+
+
+
+$( document ).ready(function() {
+
+    $("#luke").click(function(){
+        $("#luke").appendTo($("#Your-Character"));
+    })
+    $("#mara").click(function(){
+        $("#mara").appendTo($("#Your-Character"));
+    })
+    $("#kyle").click(function(){
+        $("#kyle").appendTo($("#Your-Character"));
+    })
+    $("#kiadimundi").click(function(){
+        $("#kiadimundi").appendTo($("#Your-Character"));
+    })
+});
